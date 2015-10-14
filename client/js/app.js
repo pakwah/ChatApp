c170f69ffd8bd4980326b6cbb3157b2a756ff558
collinjackson93 <collinjackson93@gmail.com>
2015-10-14 11:58:45 -0500

    Added stubs for creating users and logging in

--- client/js/app.js
+++ client/js/app.js
@@ -5,7 +5,53 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var LoginOrCreate = React.createClass({
+  render: function () {
+    return (
+      <div className="loginOrCreate">
+        <h1>Hello, world!</h1>
+        <CreateUser />
+        <Login />
+      </div>
+    );
+  }
+});
+
+var CreateUser = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var username = this.refs.username.value.trim();
+    var password = this.refs.password.value.trim();
+    if(!username || !password) {
+      return;
+    }
+    // TODO: send info to server and validate
+    this.refs.username.value = '';
+    this.refs.password.value = '';
+    return;
+  },
+  render: function() {
+    return (
+      <form className="createUser" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="username" ref="username" />
+        <input type="password" placeholder="password" ref="password" />
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+});
+
+var Login = React.createClass({
+  render: function() {
+    return (
+      <div className="login">
+        Insert things to login.
+      </div>
+    );
+  }
+});
+
 ReactDOM.render(
-    <h1>Hello!</h1>,
+    <LoginOrCreate />,
     document.getElementById('chatapp')
 );
